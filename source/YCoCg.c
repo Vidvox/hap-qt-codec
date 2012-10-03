4d09ffef5fcdf26f100a7d1955f412f458b1243d
Tom Butterworth <bangnoise@gmail.com>
2012-10-03 17:16:42 +0100

    Begin support for non-scaled YCoCg

--- source/YCoCg.c
+++ source/YCoCg.c
@@ -96,3 +96,80 @@ void ConvertCoCg_Y8888ToRGB_( uint8_t *src, uint8_t *dst, unsigned long width, u
 {
     ConvertCoCgAY8888ToRGBA(src, dst, width, height, src_rowbytes, dst_rowbytes);
 }
+
+void ConvertRGBAToCoYCgA8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    const int32_t post_bias[4] = { 512, 0, 512, 0 };
+
+    const int16_t matrix[16] = {
+         2,  1, -1,  0, // R
+         0,  2,  2,  0, // G
+        -2,  1, -1,  0, // B
+         0,  0,  0,  4  // A
+    };
+
+    ImageMath_MatrixMultiply8888(src, src_rowbytes, dst, dst_rowbytes, width, height, matrix, 4, NULL, post_bias);
+}
+
+void ConvertCoYCgA8888ToRGBA( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    const int16_t pre_bias[4] = { -128, 0, -128, 0 };
+
+    const int16_t matrix[16] = {
+         1,  0, -1,  0, // Co
+         1,  1,  1,  0, // Y
+        -1,  1, -1,  0, // Cg
+         0,  0,  0,  1  // A
+    };
+
+    ImageMath_MatrixMultiply8888(src, src_rowbytes, dst, dst_rowbytes, width, height, matrix, 1, pre_bias, NULL);
+}
+
+void ConvertBGRAToCoYCgA8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    const int32_t post_bias[4] = { 512, 512, 0, 0 };
+
+    const int16_t matrix[16] = {
+        -2,  1, -1,  0, // B
+         0,  2,  2,  0, // G
+         2,  1, -1,  0, // R
+         0,  0,  0,  4  // A
+    };
+
+    ImageMath_MatrixMultiply8888(src, src_rowbytes, dst, dst_rowbytes, width, height, matrix, 4, NULL, post_bias);
+}
+
+void ConvertCoYCgA8888ToBGRA( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    const int16_t pre_bias[4] = { -128, -128, 0, 0 };
+
+    const int16_t matrix[16] = {
+        -1,  0,  1,  0, // Co
+         1,  1,  1,  0, // Y
+        -1,  1, -1,  0, // Cg
+         0,  0,  0,  1  // A
+
+    };
+
+    ImageMath_MatrixMultiply8888(src, src_rowbytes, dst, dst_rowbytes, width, height, matrix, 1, pre_bias, NULL);
+}
+
+void ConvertBGR_ToCoYCg_8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    ConvertBGRAToCoYCgA8888(src, dst, width, height, src_rowbytes, dst_rowbytes);
+}
+
+void ConvertCoYCg_8888ToBGR_( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    ConvertCoYCgA8888ToBGRA(src, dst, width, height, src_rowbytes, dst_rowbytes);
+}
+
+void ConvertRGB_ToCoYCg_8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    ConvertRGBAToCoYCgA8888(src, dst, width, height, src_rowbytes, dst_rowbytes);
+}
+
+void ConvertCoYCg_8888ToRGB_( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes )
+{
+    ConvertCoYCgA8888ToRGBA(src, dst, width, height, src_rowbytes, dst_rowbytes);
+}
--- source/YCoCg.h
+++ source/YCoCg.h
@@ -11,6 +11,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/*
+ RGB(A) and BGR(A) to and from CoCg(A)Y
+ */
 void ConvertRGBAToCoCgAY8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
 void ConvertCoCgAY8888ToRGBA( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
 void ConvertBGRAToCoCgAY8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
@@ -20,4 +23,15 @@ void ConvertCoCg_Y8888ToBGR_( uint8_t *src, uint8_t *dst, unsigned long width, u
 void ConvertRGB_ToCoCg_Y8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
 void ConvertCoCg_Y8888ToRGB_( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
 
+/*
+ RGB(A) and BGR(A) to and from CoYCg(A)
+ */
+void ConvertRGBAToCoYCgA8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertCoYCgA8888ToRGBA( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertBGRAToCoYCgA8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertCoYCgA8888ToBGRA( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertBGR_ToCoYCg_8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertCoYCg_8888ToBGR_( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertRGB_ToCoYCg_8888( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
+void ConvertCoYCg_8888ToRGB_( uint8_t *src, uint8_t *dst, unsigned long width, unsigned long height, size_t src_rowbytes, size_t dst_rowbytes );
 #endif
