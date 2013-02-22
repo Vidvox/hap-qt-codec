750406b9853ee29b9499f28a9472746a59fdb0a1
Tom Butterworth <bangnoise@gmail.com>
2013-02-22 16:55:38 +0000

    Add license text to source files

--- source/YCoCgDXT.h
+++ source/YCoCgDXT.h
@@ -1,20 +1,46 @@
-//
-//  YCoCgDXT.h
-//  Hap Codec
-//
-//  Copyright (c) 2012 Tom Butterworth. All rights reserved.
-//  Based on code by J.M.P. van Waveren / id Software, Inc.
-//  and changes by Chris Sidhall / Electronic Arts
-//
-//  My changes are trivial:
-//      - Remove dependencies on other EAWebKit files
-//      - Mark unexported functions as static
-//      - Refactor to eliminate use of a global variable
-//      - Revert Chris Sidhall's C++ function overloading for C
-//      - Correct spelling of NVIDIA_7X_HARDWARE_BUG_FIX macro
-//      - Remove single usage of an assert macro
-//
 /*
+ YCoCgDXT.h
+ Hap Codec
+
+ Copyright (c) 2012-2013, Tom Butterworth and Vidvox LLC. All rights reserved. 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of Hap nor the name of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ 
+    Based on code by J.M.P. van Waveren / id Software, Inc.
+    and changes by Chris Sidhall / Electronic Arts
+
+    My changes are trivial:
+      - Remove dependencies on other EAWebKit files
+      - Mark unexported functions as static
+      - Refactor to eliminate use of a global variable
+      - Revert Chris Sidhall's C++ function overloading for C
+      - Correct spelling of NVIDIA_7X_HARDWARE_BUG_FIX macro
+      - Remove single usage of an assert macro
+
+ 
  Copyright (C) 2009-2011 Electronic Arts, Inc.  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
