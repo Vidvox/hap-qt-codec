c5047b08b5032698f409f0b33f8af27463c6acf0
Tom Butterworth <bangnoise@gmail.com>
2013-03-23 17:28:15 +0000

    Move snappy Mac-specific files into their own directory

--- external/snappy/snappy-mac/build-snappy.sh
+++ external/snappy/snappy-mac/build-snappy.sh
@@ -9,7 +9,7 @@
 mkdir -p "$DERIVED_FILE_DIR/snappy-source"
 mkdir -p "$DERIVED_FILE_DIR/snappy-install"
 mkdir -p "$BUILT_PRODUCTS_DIR/include"
-cp -r "$SRCROOT/snappy-source/" "$DERIVED_FILE_DIR/snappy-source/"
+cp -r "$SRCROOT/../snappy-source/" "$DERIVED_FILE_DIR/snappy-source/"
 cd "$DERIVED_FILE_DIR/snappy-source/"
 ./configure CC="gcc -arch $ARCHS" CXX="g++ -arch $ARCHS" CPP="gcc -E" CXXCPP="g++ -E" CFLAGS="-O2 -DNDEBUG" CXXFLAGS="-O2 -DNDEBUG" LDFLAGS="-Wl,-install_name,@rpath/libsnappy.1.dylib" --enable-static=NO --disable-gtest --prefix="$DERIVED_FILE_DIR/snappy-install/" -C
 make install
