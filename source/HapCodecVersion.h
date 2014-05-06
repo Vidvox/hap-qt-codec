c607ba34d4c19ca4a428df0e588e2bc055c329ec
Tom Butterworth <bangnoise@gmail.com>
2014-05-06 18:04:17 +0100

    Bump version to 3

--- source/HapCodecVersion.h
+++ source/HapCodecVersion.h
@@ -26,7 +26,7 @@
  */
 
 // The high word is the codecInterfaceVersion
-#define kHapDecompressorVersion		(0x00040002)
+#define kHapDecompressorVersion		(0x00040003)
 
 // The high word is the codecInterfaceVersion
-#define kHapCompressorVersion		(0x00040002)
+#define kHapCompressorVersion		(0x00040003)
