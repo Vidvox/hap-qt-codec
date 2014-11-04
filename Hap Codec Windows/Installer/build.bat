000afd79bcf91904137790433564d9ced604e865
Tom Butterworth <bangnoise@gmail.com>
2014-11-04 13:55:23 +0000

    Update projects for Visual Studio 2013 and WiX 3.9

--- Hap Codec Windows/Installer/build.bat
+++ Hap Codec Windows/Installer/build.bat
@@ -1,3 +1,3 @@
 MSBuild "..\Hap Codec.sln" /p:Configuration=RELEASE
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" HapQuickTimeSetup.wxs
-"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" -ext WixUIExtension HapQuickTimeSetup.wixobj
+"C:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" HapQuickTimeSetup.wxs
+"C:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" -ext WixUIExtension HapQuickTimeSetup.wixobj
