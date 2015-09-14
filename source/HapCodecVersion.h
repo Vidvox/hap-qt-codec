2886505ed25a6490d19a492864f17635ea579d8c
Tom Butterworth <bangnoise@gmail.com>
2015-09-14 13:28:45 +0100

    Version to 7, update date in LICENSE

--- source/HapCodecVersion.h
+++ source/HapCodecVersion.h
@@ -26,7 +26,7 @@
  */
 
 // The high word is the codecInterfaceVersion
-#define kHapDecompressorVersion		(0x00040006)
+#define kHapDecompressorVersion		(0x00040007)
 
 // The high word is the codecInterfaceVersion
-#define kHapCompressorVersion		(0x00040006)
+#define kHapCompressorVersion		(0x00040007)
